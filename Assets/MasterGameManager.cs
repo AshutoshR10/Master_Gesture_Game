43286fcc0a4b1914bd103e828f5531530c762194
Koushik <koushik.roy@studiokrew.com>
2025-08-28 19:28:14 +0530

    Changes inn the naming of the scene

--- Assets/MasterGameManager.cs
+++ Assets/MasterGameManager.cs
@@ -3,6 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MasterGameManager : MonoBehaviour
 {
+
+    [ContextMenu("Test Load Scene")]
+    public void TestLoadSceneDino()
+    {
+        LoadGameScene("DINO");
+    }
+
+    [ContextMenu("Test Load Space")]
+    public void TestLoadSceneSpace()
+    {
+        LoadGameScene("SPACE");
+    }
+
     public void LoadGameScene(string keyCode)
     {
         switch (keyCode)
